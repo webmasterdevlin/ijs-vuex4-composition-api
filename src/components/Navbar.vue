a298c7536bf0e924f4fadf2c0d6f3ef4957b1049
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-29 10:15:38 +0200

    update

---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sticky-top">
+    <nav
+      class="
+        navbar
+        shadow
+        bg-white
+        rounded
+        justify-content-between
+        flex-nowrap flex-row
+      "
+      style="margin-bottom: 2rem"
+    >
+      <div class="container">
+        <router-link to="/" class="navbar-brand float-left">LOGO</router-link>
+        <ul class="nav navbar-nav d-flex flex-row bd-highlight">
+          <li class="nav-item me-5">
+            <router-link to="/heroes" exact class="nav-link"
+              >Heroes</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link to="/anti-heroes" exact class="nav-link">
+              Anti-Heroes</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
