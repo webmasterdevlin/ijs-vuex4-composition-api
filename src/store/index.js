5b540c7a10149f9c4907ec9e3c27fc1e62bd83ea
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-29 11:52:24 +0200

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,9 @@
-import { createStore } from 'vuex'
+import { createStore, createLogger } from "vuex";
+import heroModule from "./hero";
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+export const store = createStore({
   modules: {
-  }
-})
+    heroModule,
+  },
+  plugins: [createLogger()],
+});
