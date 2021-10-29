669fc938a9473144b00b2be7774cd46e01123b00
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-29 11:38:42 +0200

    update

--- src/store/hero/actions.js
+++ src/store/hero/actions.js
@@ -1,3 +1,11 @@
 import * as types from "./types";
+import { get } from "@/api/axiosConfig";
 
+export function getHeroesAction({ commit }) {
+  commit(types.IS_LOADING_HERO, true);
 
+  return get("heroes")
+    .then(({ data }) => commit(types.GET_HEROES, data))
+    .catch((e) => console.log(e.message))
+    .finally(() => commit(types.IS_LOADING_HERO, false));
+}
