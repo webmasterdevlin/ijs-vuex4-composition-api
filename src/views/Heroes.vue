5271c203b33f2e2c6e9f252a4ec98598dbfb5b2e
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-10-29 12:04:22 +0200

    update

--- src/views/Heroes.vue
+++ src/views/Heroes.vue
@@ -1,15 +1,47 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div class="container-fluid">
+    <h1>Heroes Works!</h1>
+    <section>
+      <div v-if="heroes.length > 0">
+        <div
+          class="card mt-3"
+          style="width: auto"
+          v-for="hero in heroes"
+          :key="hero.id"
+        >
+          <div class="card-header">
+            <h3 class="card-title">{{ hero.firstName }} {{ hero.lastName }}</h3>
+            <h5 class="card-subtitle mb-2 text-muted">{{ hero.house }}</h5>
+            <p class="card-text">{{ hero.knownAs }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 // Vuejs 2 implemetations
+import { mapGetters, mapActions } from "vuex";
+
 export default {
   name: "Heroes",
   data() {
     return {
       message: "Heroes Works!",
     };
+  },
+  computed: {
+    ...mapGetters("heroModule", {
+      heroes: "heroes",
+      isLoading: "isLoading",
+    }),
+  },
+  methods: {
+    ...mapActions("heroModule", ["getHeroesAction"]),
+  },
+  mounted() {
+    this.getHeroesAction();
   },
 };
 </script>
